4725dd4f87f11e8a5c89704be6d3b744dc4b69d0
sm-alfariz <samman.alfariz@gmail.com>
2016-10-04 05:35:12 +0800

    fix-url/route di simple kan

--- urls.php
+++ urls.php
@@ -3,29 +3,16 @@ require_once __DIR__.'/bootstrap.php'; //dipisahkan utk urusan load composer sam
 use App\RouteCollection;
 use App\Router;
 use App\Route;
-$collection = new RouteCollection();
-$collection->prosesRoute(new Route('/', array('ctrl' => 'App\controllers\Berita::index','methods' => 'GET','name'=>'get.home')));
-$collection->prosesRoute(new Route('/berita', array('ctrl' => 'App\controllers\Berita::index','methods' => 'GET','name'=>'get.berita')));
-$collection->prosesRoute(new Route('/berita/:slug', array('ctrl' => 'App\controllers\Berita::BacaBerita','methods' => 'GET','name'=>'get.beritaBaca')));
 
-$router = new Router($collection);
+$app = new RouteCollection();
+$app->prosesRoute(new Route('/', array('ctrl' => 'App\controllers\Berita::index','methods' => 'GET','name'=>'get.home')));
+$app->prosesRoute(new Route('/berita', array('ctrl' => 'App\controllers\Berita::index','methods' => 'GET','name'=>'get.berita')));
+$app->prosesRoute(new Route('/berita/:slug', array('ctrl' => 'App\controllers\Berita::BacaBerita','methods' => 'GET','name'=>'get.beritaBaca')));
+
+$router = new Router($app);
 $router->setBasePath('');
 $route = $router->matchCurrentRequest();
 
-
-//var_dump($route);
-/**** jika ingin diLoad route nya dengan yml/yaml **/
-//pastikan final class RouteYml di load serta class route lainnya 
-// use App\RouteYml;
-// use App\RouteCollection;
-// use App\Router;
-// use App\Route;
-// $config = RouteYml::loadFromFile(__DIR__.'/route.yaml');
-// $router = Router::parseConfig($config);
-
-// $route = $router->matchCurrentRequest();
-// $router->matchCurrentRequest();
-// //var_dump($router);
 if(!$route){
 	header("HTTP/1.0 404 Not Found");	
 	//var_dump($route);
